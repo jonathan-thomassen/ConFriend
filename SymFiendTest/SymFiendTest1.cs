6311fa2c8a213922b42ddd86fa912b3b644c104f
Jonathan Yannick Thomassen <jona97g8@edu.easj.dk>
2021-04-27 10:14:34 +0200

    Unit Test test

---
+++ SymFiendTest/SymFiendTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SymFiendTest
+{
+    [TestClass]
+    public class SymFiendTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
