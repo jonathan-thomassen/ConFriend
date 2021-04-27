1edee18bb1b7f2ddf858506c3076f6393f25dd29
Jonathan Yannick Thomassen <73933132+jonathan-thomassen@users.noreply.github.com>
2021-04-27 14:27:23 +0200

    Update and rename SymFiendTest1.cs to ConFriendTest1.cs

--- SymFiendTest/ConFriendTest1.cs
+++ SymFiendTest/ConFriendTest1.cs
@@ -1,9 +1,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace SymFiendTest
+namespace ConFriendTest
 {
     [TestClass]
-    public class SymFiendTest1
+    public class ConFriendTest1
     {
         [TestMethod]
         public void TestMethod1()
