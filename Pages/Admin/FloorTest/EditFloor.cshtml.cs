7d6520a8bb01e3b8093875c83c52bd39125743f5
Mads Horsted Brunebjerg <mads81i7@edu.easj.dk>
2021-05-04 11:00:33 +0200

    FloorTest CRUD-pages Done

---
+++ Pages/Admin/FloorTest/EditFloor.cshtml.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConFriend.Interfaces;
+using ConFriend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ConFriend.Pages.Admin.FloorTest
+{
+    public class EditFloorModel : PageModel
+    {
+        [BindProperty] public Floor NewFloor { get; set; }
+        public bool done { get; set; }
+        private ICrudService<Floor> floorService;
+        public EditFloorModel(ICrudService<Floor> fService)
+        {
+            floorService = fService;
+            done = false;
+        }
+        public void OnGet(int fId)
+        {
+            NewFloor = floorService.GetFromId(fId);
+        }
+
+        public void OnPost()
+        {
+            floorService.Update(NewFloor);
+            done = true;
+        }
+    }
+}
