c9d0a0406251385cfab2819997d3d0600c39b36a
Mads Horsted Brunebjerg <mads81i7@edu.easj.dk>
2021-05-04 14:40:48 +0200

    Theme, EventTheme, Feature, RoomFeature, SeatCategory, RoomSeatCategory and SeatCategoryTaken Models have been added

---
+++ Models/Feature.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConFriend.Models
+{
+    public class Feature: IModel
+    {
+        public int FeatureId { get; set; }
+        public string Name { get; set; }    
+        public string ToSQL()
+        {
+            return $"Name = {Name}";
+        }
+
+        public string Identity()
+        {
+            return $"FeatureId = {FeatureId}";
+        }
+    }
+}
---
+++ Models/SeatCategory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConFriend.Models
+{
+    public class SeatCategory : IModel
+    {
+        public int SeatCategoryId { get; set; }
+        public string NameKey { get; set; }
+        public string ToSQL()
+        {
+            return $"NameKey = {NameKey}";
+        }
+
+        public string Identity()
+        {
+            return $"SeatCategoryId = {SeatCategoryId}";
+        }
+    }
+}
