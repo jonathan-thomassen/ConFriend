085e5237e2a6294183eba66fc5b2696b4d3cc8c9
Jonathan Yannick Thomassen <jona97g8@edu.easj.dk>
2021-05-05 14:14:56 +0200

    CreateRoom (Doesn't work)

--- Pages/Admin/RoomTest/CreateRoom.cshtml.cs
+++ Pages/Admin/RoomTest/CreateRoom.cshtml.cs
@@ -6,32 +6,55 @@ using ConFriend.Interfaces;
 using ConFriend.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ConFriend.Pages.Admin.RoomTest
 {
     public class CreateRoomModel : PageModel
     {
         [BindProperty] public Room NewRoom { get; set; }
-        public bool done { get; set; }
-        private ICrudService<Room> roomService;
-        public CreateRoomModel(ICrudService<Room> rService)
+        private Room _room;
+        public List<Floor> Floors { get; set; }
+        public List<Venue> Venues { get; set; }
+        public SelectList SelectListFloors { get; set; }
+        public SelectList SelectListVenues { get; set; }
+        public bool VenueSet;
+
+        private readonly ICrudService<Room> _roomService;
+        private readonly ICrudService<Venue> _venueService;
+        private readonly ICrudService<Floor> _floorService;
+
+        public CreateRoomModel(ICrudService<Room> roomService, ICrudService<Floor> floorService, ICrudService<Venue> venueService)
         {
-            roomService = rService;
-            NewRoom = new Room();
-            done = false;
+            _roomService = roomService;
+            _floorService = floorService;
+            _venueService = venueService;
         }
-        public void OnGet()
+        public IActionResult OnGet()
         {
+            Venues = _venueService.GetAll();
+            SelectListVenues = new SelectList(Venues, nameof(Venue.VenueId), nameof(Venue.Name));
+            VenueSet = false;
+            return Page();
         }
 
         public IActionResult OnPost()
         {
+            if (!VenueSet)
+            {
+                Floors = _floorService.GetAll();
+                VenueSet = true;
+                SelectListFloors = new SelectList(Floors.FindAll(floor => floor.VenueId.Equals(NewRoom.VenueId)), nameof(Floor.VenueId), nameof(Floor.Name));
+                _room = NewRoom;
+
+                return Page();
+            }
+            
             if (!ModelState.IsValid)
             {
                 return Page();
             }
-            roomService.Create(NewRoom);
-            done = true;
+            _roomService.Create(NewRoom);
             return Page();
         }
     }
