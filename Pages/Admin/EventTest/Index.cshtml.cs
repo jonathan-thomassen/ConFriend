68168db4f0a53b568e588263680d7fb93fa5fcbc
Mads Horsted Brunebjerg <mads81i7@edu.easj.dk>
2021-05-05 11:26:26 +0200

    EventTest/Index Implemented

---
+++ Pages/Admin/EventTest/Index.cshtml.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConFriend.Interfaces;
+using ConFriend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ConFriend.Pages.Admin.EventTest
+{
+    public class IndexModel : PageModel
+    {
+        public List<Event> Events { get; private set; }
+
+        private ICrudService<Event> eventService;
+
+        public IndexModel(ICrudService<Event> eService)
+        {
+            this.eventService = eService;
+        }
+        public void OnGet()
+        {
+            Events = eventService.GetAll();
+        }
+    }
+}
