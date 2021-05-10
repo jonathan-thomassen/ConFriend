15c8c3562c21cacf017d70c873cd2a7c16fadc51
GitG0at <rgo2@outlook.dk>
2021-05-10 13:42:03 +0200

    enrollment

--- Models/Enrollment.cs
+++ Models/Enrollment.cs
@@ -8,6 +8,10 @@ namespace ConFriend.Models
         public DateTime SignUpTime { get; set; }
         public User User { get; set; }
         public Event Event { get; set; }
+        public int userId { get; set; }
+        public int eventId { get; set; }
+        
+        
         public string ToSQL()
         {
             return $"EventId = {Event.EventId}, UserId = {User.UserId}, SignUpTime = {SignUpTime}";
