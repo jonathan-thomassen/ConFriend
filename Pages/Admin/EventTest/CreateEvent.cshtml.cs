9a423d0aabc830b49d92e6f196cd73dc9736f510
Jonathan Yannick Thomassen <jona97g8@edu.easj.dk>
2021-05-11 15:02:22 +0200

    CreateEvent: Nu med selectlist

--- Pages/Admin/EventTest/CreateEvent.cshtml.cs
+++ Pages/Admin/EventTest/CreateEvent.cshtml.cs
@@ -6,33 +6,96 @@ using ConFriend.Interfaces;
 using ConFriend.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ConFriend.Pages.Admin.EventTest
 {
     public class CreateEventModel : PageModel
     {
         [BindProperty] public Event NewEvent { get; set; }
-        private ICrudService<Event> eventService;
-        public CreateEventModel(ICrudService<Event> eService)
+        public List<Room> Rooms;
+        public List<Venue> Venues;
+        public List<Conference> Conferences;
+        public List<Models.Speaker> Speakers;
+
+        public SelectList SelectListVenues;
+        public SelectList SelectListRooms;
+        public SelectList SelectListSpeakers;
+        public SelectList SelectListConferences;
+
+        public int VenueId;
+
+        private readonly ICrudService<Event> _eventService;
+        private readonly ICrudService<Room> _roomService;
+        private readonly ICrudService<Venue> _venueService;
+        private readonly ICrudService<Conference> _conferenceService;
+        private readonly ICrudService<Models.Speaker> _speakerService;
+
+        public CreateEventModel(ICrudService<Event> eventService, ICrudService<Room> roomService, ICrudService<Venue> venueService, ICrudService<Conference> conferenceService, ICrudService<Models.Speaker> speakerService)
         {
-            eventService = eService;
-            eventService.Init(ModelTypes.Event);
-            NewEvent = new Event();
-            //NewEvent.StartTime = DateTime.Now;
+            _eventService = eventService;
+            _roomService = roomService;
+            _venueService = venueService;
+            _conferenceService = conferenceService;
+            _speakerService = speakerService;
+
+            _eventService.Init(ModelTypes.Event);
+            _roomService.Init(ModelTypes.Room);
+            _venueService.Init(ModelTypes.Venue);
+            _conferenceService.Init(ModelTypes.Conference);
+            _speakerService.Init(ModelTypes.Speaker);
         }
         public void OnGet()
         {
-          
+            NewEvent = new Event();
+            Venues = _venueService.GetAll();
+            Speakers = _speakerService.GetAll();
+            Conferences = _conferenceService.GetAll();
+
+            Venues.Insert(0, new Venue());
+            Speakers.Insert(0, new Models.Speaker());
+            Conferences.Insert(0, new Conference());
+
+            SelectListVenues = new SelectList(Venues, nameof(Venue.VenueId), nameof(Venue.Name));
+            SelectListSpeakers = new SelectList(Speakers, nameof(Models.Speaker.SpeakerId), nameof(Models.Speaker.FullName));
+            SelectListConferences = new SelectList(Conferences, nameof(Conference.ConferenceId), nameof(Conference.Name));
         }
 
-        public IActionResult OnPost()
+        public IActionResult OnPost(int? venueId, int? roomId)
         {
-            if (!ModelState.IsValid)
+            if (roomId == 0)
             {
+                Venues = _venueService.GetAll();
+
+                Rooms = _roomService.GetAll();
+                Speakers = _speakerService.GetAll();
+                Conferences = _conferenceService.GetAll();
+
+                Rooms.Insert(0, new Room());
+                Speakers.Insert(0, new Models.Speaker());
+                Conferences.Insert(0, new Conference());
+
+                SelectListRooms = new SelectList(Rooms.FindAll(room => room.VenueId.Equals(venueId) || room.VenueId == 0), nameof(Room.RoomId), nameof(Room.Name));
+                SelectListSpeakers = new SelectList(Speakers, nameof(Models.Speaker.SpeakerId), nameof(Models.Speaker.FullName));
+                SelectListConferences = new SelectList(Conferences, nameof(Conference.ConferenceId), nameof(Conference.Name));
+                
+                VenueId = (int)venueId;
+                ModelState.Clear();
                 return Page();
             }
-            eventService.Create(NewEvent);
+
+            NewEvent.RoomId = (int)roomId;
+
+            if (!ModelState.IsValid)
+                return RedirectToPage("Index");
+
+            _eventService.Create(NewEvent);
             return RedirectToPage("Index");
+        }
+
+        public void OnPostClearVenueChoice()
+        {
+            OnGet();
         }
     }
 }
