1938e6eb48142ba3c23a7a4a4dbb4403654b7450
Jonathan Yannick Thomassen <jona97g8@edu.easj.dk>
2021-05-17 14:14:05 +0200

    Forside

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -12,18 +12,22 @@ namespace ConFriend.Pages
     {
         private readonly ICrudService<User> _userService;
         private readonly ICrudService<UserConferenceBinding> _ucBindingService;
+        private readonly ICrudService<Conference> _conferenceService;
         private readonly SessionService _sessionService;
 
         public new User User;
+        public Conference Conference;
         public UserType UserType;
 
-        public IndexModel(ICrudService<User> userService, ICrudService<UserConferenceBinding> ucBindingService, SessionService sessionService)
+        public IndexModel(ICrudService<User> userService, ICrudService<UserConferenceBinding> ucBindingService, ICrudService<Conference> conferenceService, SessionService sessionService)
         {
             _userService = userService;
             _ucBindingService = ucBindingService;
+            _conferenceService = conferenceService;
 
             _userService.Init(ModelTypes.User);
             _ucBindingService.Init(ModelTypes.UserConferenceBinding);
+            _conferenceService.Init(ModelTypes.Conference);
 
             _sessionService = sessionService;
         }
@@ -36,13 +40,16 @@ namespace ConFriend.Pages
             var userId = _sessionService.GetUserId(HttpContext.Session);
             var conId = _sessionService.GetConferenceId(HttpContext.Session);
 
+            if (conId != null)
+                Conference = await _conferenceService.GetFromId((int)conId);
+            if (userId != null)
+                User = await _userService.GetFromId((int)userId);
+
             if (conId != null && userId != null)
             {
                 int? bindingId = _ucBindingService.GetAll().Result.FindAll(binding => binding.UserId.Equals(userId)).Find(binding => binding.ConferenceId.Equals(conId))?.UserConferenceBindingId;
                 if (bindingId != null)
-                    User = await _userService.GetFromId((int)userId);
-                    UserType = _ucBindingService.GetFromId((int)bindingId).Result.UserType;
-                return Page();
+                    UserType = _ucBindingService.GetFromId((int) bindingId).Result.UserType;
             }
 
             return Page();
